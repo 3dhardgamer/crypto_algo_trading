f3d24f3ef80870a154c0c7435c48705fc4105767
3dhardgamer <artur.turbas@gmail.com>
2018-01-23 18:19:38 -0200

    Initialization of pull-request to cex.io.

---
+++ cex.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+
+
+def get_ohlcv(symbol, currency, start, end = datetime.now()):
+    '''
+    get_ohlcv(symbol, currency, start, end): connects to CEX.IO api and retrieves the OHLCV data.
+
+    parameters:
+    symbol - Cryptocurrency acronym
+    currency - Curreny acronym
+    start - Initial data
+    end - Final data
+    '''
+
+    end =
+    start =
+    url = 'https://cex.io/api/ohlcv/hd/{date}/{symbol1}/{symbol2}'.format()
+    print('')
